dd87428eb35f23928a7167084552cf6b25646381
luke biggerstaff <luke.biggerstaff@gmail.com>
2018-03-02 08:21:32 -0600

    renamed folders from usda to foodviz

--- foodviz/foodviz/settings.py
+++ foodviz/foodviz/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'usda.urls'
+ROOT_URLCONF = 'foodviz.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'usda.wsgi.application'
+WSGI_APPLICATION = 'foodviz.wsgi.application'
 
 
 # Database
