ab62bf8b12c0b9713e83eb79c60decfa48feca31
luke biggerstaff <luke.biggerstaff@gmail.com>
2018-03-06 15:10:52 -0600

    updated Search component to store search result data in state and to pass call back function to input

--- foodviz/frontend/src/components/SearchInput.js
+++ foodviz/frontend/src/components/SearchInput.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 class SearchInput extends Component {
   render() {
     return (
-      <input className="form-control" />
+      <input value={this.props.value} onChange={this.props.onChange} 
+      placeholder="Type to begin searching..." className="form-control form-control-lg" />
     );
   }
 }
