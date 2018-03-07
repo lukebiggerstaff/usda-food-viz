8c71744d5bd1bae67e040c41166b69398d2fe851
luke biggerstaff <luke.biggerstaff@gmail.com>
2018-03-07 15:16:58 -0600

    updated placeholder text to encourage clicking on charts for more info

--- foodviz/frontend/src/components/SearchInput.js
+++ foodviz/frontend/src/components/SearchInput.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import '../styles/SearchInput.css';
 
 class SearchInput extends Component {
   render() {
     return (
       <input value={this.props.value} onChange={this.props.onChange} 
-      placeholder="Type to begin searching..." className="form-control form-control-lg" />
+      placeholder="Type to begin searching... and click the chart for more info" className="form-control form-control-lg" />
     );
   }
 }
